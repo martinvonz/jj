fbc13440a136f92b67296cd7c792530b11a054a1
Martin von Zweigbergk <martinvonz@google.com>
2022-02-05 13:44:24 -0800

    tests: don't create workspaces in tree-merge tests

--- lib/tests/test_merge_trees.rs
+++ lib/tests/test_merge_trees.rs
@@ -32,8 +32,8 @@ fn test_same_type(use_git: bool) {
     // using only normal files in all trees (no symlinks, no trees, etc.).
 
     let settings = testutils::user_settings();
-    let test_workspace = testutils::init_workspace(&settings, use_git);
-    let repo = &test_workspace.repo;
+    let test_repo = testutils::init_repo(&settings, use_git);
+    let repo = &test_repo.repo;
     let store = repo.store();
 
     // The file name encodes the state in the base and in each side ("_" means
@@ -232,8 +232,8 @@ fn test_subtrees(use_git: bool) {
     // Tests that subtrees are merged.
 
     let settings = testutils::user_settings();
-    let test_workspace = testutils::init_workspace(&settings, use_git);
-    let repo = &test_workspace.repo;
+    let test_repo = testutils::init_repo(&settings, use_git);
+    let repo = &test_repo.repo;
     let store = repo.store();
 
     let write_tree = |paths: Vec<&str>| -> Tree {
@@ -289,8 +289,8 @@ fn test_subtree_becomes_empty(use_git: bool) {
     // Tests that subtrees that become empty are removed from the parent tree.
 
     let settings = testutils::user_settings();
-    let test_workspace = testutils::init_workspace(&settings, use_git);
-    let repo = &test_workspace.repo;
+    let test_repo = testutils::init_repo(&settings, use_git);
+    let repo = &test_repo.repo;
     let store = repo.store();
 
     let write_tree = |paths: Vec<&str>| -> Tree {
@@ -322,8 +322,8 @@ fn test_types(use_git: bool) {
     // conflicts survive the roundtrip to the store.
 
     let settings = testutils::user_settings();
-    let test_workspace = testutils::init_workspace(&settings, use_git);
-    let repo = &test_workspace.repo;
+    let test_repo = testutils::init_repo(&settings, use_git);
+    let repo = &test_repo.repo;
     let store = repo.store();
 
     let mut base_tree_builder = store.tree_builder(store.empty_tree_id().clone());
@@ -447,8 +447,8 @@ fn test_types(use_git: bool) {
 #[test_case(true ; "git backend")]
 fn test_simplify_conflict(use_git: bool) {
     let settings = testutils::user_settings();
-    let test_workspace = testutils::init_workspace(&settings, use_git);
-    let repo = &test_workspace.repo;
+    let test_repo = testutils::init_repo(&settings, use_git);
+    let repo = &test_repo.repo;
     let store = repo.store();
 
     let write_tree = |contents: &str| -> Tree {
@@ -571,8 +571,8 @@ fn test_simplify_conflict(use_git: bool) {
 #[test_case(true ; "git backend")]
 fn test_simplify_conflict_after_resolving_parent(use_git: bool) {
     let settings = testutils::user_settings();
-    let test_workspace = testutils::init_workspace(&settings, use_git);
-    let repo = &test_workspace.repo;
+    let test_repo = testutils::init_repo(&settings, use_git);
+    let repo = &test_repo.repo;
 
     // Set up a repo like this:
     // D
