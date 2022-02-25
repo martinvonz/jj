fbe8eb47db9e7af3095a2f5f35b2fdc89bb8f4ed
Cole Mickens <cole.mickens@gmail.com>
2022-02-25 15:08:18 -0800

    lib/settings: try to load from platform config_dir

--- lib/src/settings.rs
+++ lib/src/settings.rs
@@ -24,6 +24,8 @@ pub struct RepoSettings {
     _config: config::Config,
 }
 
+const TOO_MUCH_CONFIG_ERROR: &str = "Both `$HOME/.jjconfig` and `$XDG_CONFIG_HOME/jj/config.toml` were found, please remove one.";
+
 impl UserSettings {
     pub fn from_config(config: config::Config) -> Self {
         UserSettings { config }
@@ -32,9 +34,28 @@ impl UserSettings {
     pub fn for_user() -> Result<Self, config::ConfigError> {
         let mut config = config::Config::new();
 
+        let loaded_from_config_dir = match dirs::config_dir() {
+            None => false,
+            Some(config_dir) => {
+                let p = config_dir.join("jj/config.toml");
+                let exists = p.exists();
+                config.merge(
+                    config::File::from(p)
+                        .required(false)
+                        .format(config::FileFormat::Toml),
+                )?;
+                exists
+            }
+        };
+
         if let Some(home_dir) = dirs::home_dir() {
+            let p = home_dir.join(".jjconfig");
+            // we already loaded from the new location, prevent user confusion and make them remove the old one:
+            if loaded_from_config_dir && p.exists() {
+                return Err(config::ConfigError::Message(TOO_MUCH_CONFIG_ERROR.to_string()));
+            }
             config.merge(
-                config::File::from(home_dir.join(".jjconfig"))
+                config::File::from(p)
                     .required(false)
                     .format(config::FileFormat::Toml),
             )?;
