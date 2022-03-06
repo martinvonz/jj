57214365586697c1a72def517dbf551e64e3eb87
Martin von Zweigbergk <martinvonz@google.com>
2022-03-05 19:32:20 -0800

    tests: use `insta` crate where appropriate
    Thanks to @arxanas for the suggestion.

--- tests/test_global_opts.rs
+++ tests/test_global_opts.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use jujutsu::testutils::TestEnvironment;
+use jujutsu::testutils::{get_stdout_string, TestEnvironment};
 
 #[test]
 fn test_no_commit_working_copy() {
@@ -25,14 +25,16 @@ fn test_no_commit_working_copy() {
     let repo_path = test_env.env_root().join("repo");
 
     std::fs::write(repo_path.join("file"), "initial").unwrap();
-    let expected_output = "@ 1e9ff0ea7220c37a1d2c4aab153e238c12ff3cd0
-o 0000000000000000000000000000000000000000
-";
-    test_env
+    let assert = test_env
         .jj_cmd(&repo_path, &["log", "-T", "commit_id"])
         .assert()
-        .success()
-        .stdout(expected_output);
+        .success();
+    let stdout_string = get_stdout_string(&assert);
+    insta::assert_snapshot!(stdout_string, @r###"
+    @ 1e9ff0ea7220c37a1d2c4aab153e238c12ff3cd0
+    o 0000000000000000000000000000000000000000
+    "###);
+
 
     // Modify the file. With --no-commit-working-copy, we still get the same commit
     // ID.
@@ -44,35 +46,37 @@ o 0000000000000000000000000000000000000000
         )
         .assert()
         .success()
-        .stdout(expected_output);
+        .stdout(stdout_string);
 
     // But without --no-commit-working-copy, we get a new commit ID.
-    let expected_output = "@ cc12440b719c67fcd8c55848eb345f67b6e2d9f1
-o 0000000000000000000000000000000000000000
-";
-    test_env
+    let assert = test_env
         .jj_cmd(&repo_path, &["log", "-T", "commit_id"])
         .assert()
-        .success()
-        .stdout(expected_output);
+        .success();
+    insta::assert_snapshot!(get_stdout_string(&assert), @r###"
+    @ cc12440b719c67fcd8c55848eb345f67b6e2d9f1
+    o 0000000000000000000000000000000000000000
+    "###);
 }
 
 #[test]
 fn test_repo_arg_with_init() {
     let test_env = TestEnvironment::default();
-    test_env
+    let assert = test_env
         .jj_cmd(test_env.env_root(), &["init", "-R=.", "repo"])
         .assert()
-        .failure()
-        .stdout("Error: '--repository' cannot be used with 'init'\n");
+        .failure();
+    insta::assert_snapshot!(get_stdout_string(&assert), @"Error: '--repository' cannot be used with 'init'
+");
 }
 
 #[test]
 fn test_repo_arg_with_git_clone() {
     let test_env = TestEnvironment::default();
-    test_env
+    let assert = test_env
         .jj_cmd(test_env.env_root(), &["git", "clone", "-R=.", "remote"])
         .assert()
-        .failure()
-        .stdout("Error: '--repository' cannot be used with 'git clone'\n");
+        .failure();
+    insta::assert_snapshot!(get_stdout_string(&assert), @"Error: '--repository' cannot be used with 'git clone'
+");
 }
