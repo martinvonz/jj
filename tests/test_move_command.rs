dc41c4bfe4348f5262b47481a6d8a29fcb4b2ce1
Martin von Zweigbergk <martinvonz@google.com>
2022-04-06 10:05:21 -0700

    tests: add tests of `jj move`

---
+++ tests/test_move_command.rs
@@ -0,0 +1,145 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::common::TestEnvironment;
+
+pub mod common;
+
+#[test]
+fn test_move() {
+    let test_env = TestEnvironment::default();
+    test_env.jj_cmd_success(test_env.env_root(), &["init", "repo", "--git"]);
+    let repo_path = test_env.env_root().join("repo");
+
+    // Create history like this:
+    // F
+    // |
+    // E C
+    // | |
+    // D B
+    // |/
+    // A
+    //
+    // When moving changes between e.g. C and F, we should not get unrelated changes
+    // from B and D.
+    test_env.jj_cmd_success(&repo_path, &["branch", "a"]);
+    std::fs::write(repo_path.join("file1"), "a\n").unwrap();
+    std::fs::write(repo_path.join("file2"), "a\n").unwrap();
+    std::fs::write(repo_path.join("file3"), "a\n").unwrap();
+    test_env.jj_cmd_success(&repo_path, &["new"]);
+    test_env.jj_cmd_success(&repo_path, &["branch", "b"]);
+    std::fs::write(repo_path.join("file3"), "b\n").unwrap();
+    test_env.jj_cmd_success(&repo_path, &["new"]);
+    test_env.jj_cmd_success(&repo_path, &["branch", "c"]);
+    std::fs::write(repo_path.join("file1"), "c\n").unwrap();
+    test_env.jj_cmd_success(&repo_path, &["co", "a"]);
+    test_env.jj_cmd_success(&repo_path, &["new"]);
+    test_env.jj_cmd_success(&repo_path, &["branch", "d"]);
+    std::fs::write(repo_path.join("file3"), "d\n").unwrap();
+    test_env.jj_cmd_success(&repo_path, &["new"]);
+    test_env.jj_cmd_success(&repo_path, &["branch", "e"]);
+    std::fs::write(repo_path.join("file2"), "e\n").unwrap();
+    test_env.jj_cmd_success(&repo_path, &["new"]);
+    test_env.jj_cmd_success(&repo_path, &["branch", "f"]);
+    std::fs::write(repo_path.join("file2"), "f\n").unwrap();
+    // Test the setup
+    let template = r#"commit_id.short() " " branches"#;
+    let stdout = test_env.jj_cmd_success(&repo_path, &["log", "-T", template]);
+    insta::assert_snapshot!(stdout, @r###"
+    @ 0d7353584003 f
+    o e9515f21068c e
+    o bdd835cae844 d
+    | o caa4d0b23201 c
+    | o 55171e33db26 b
+    |/  
+    o 3db0a2f5b535 a
+    o 000000000000 
+    "###);
+
+    // Doesn't do anything without arguments
+    // TODO: We should make this error more helpful (saying that --from and/or --to
+    // are required)
+    let stdout = test_env.jj_cmd_failure(&repo_path, &["move"]);
+    insta::assert_snapshot!(stdout, @"Error: Source and destination cannot be the same.
+");
+    let stdout = test_env.jj_cmd_success(&repo_path, &["log", "-T", template]);
+    insta::assert_snapshot!(stdout, @r###"
+    @ 0d7353584003 f
+    o e9515f21068c e
+    o bdd835cae844 d
+    | o caa4d0b23201 c
+    | o 55171e33db26 b
+    |/  
+    o 3db0a2f5b535 a
+    o 000000000000 
+    "###);
+
+    // Can move from sibling, which results in the source being abandoned
+    let stdout = test_env.jj_cmd_success(&repo_path, &["move", "--from", "c"]);
+    insta::assert_snapshot!(stdout, @r###"
+    Working copy now at: 1c03e3d3c63f 
+    Added 0 files, modified 1 files, removed 0 files
+    "###);
+    let stdout = test_env.jj_cmd_success(&repo_path, &["log", "-T", template]);
+    insta::assert_snapshot!(stdout, @r###"
+    @ 1c03e3d3c63f f
+    o e9515f21068c e
+    o bdd835cae844 d
+    | o 55171e33db26 b c
+    |/  
+    o 3db0a2f5b535 a
+    o 000000000000 
+    "###);
+    // The change from the source has been applied
+    let stdout = test_env.jj_cmd_success(&repo_path, &["print", "file1"]);
+    insta::assert_snapshot!(stdout, @"c
+");
+    // File `file2`, which was not changed in source, is unchanged
+    let stdout = test_env.jj_cmd_success(&repo_path, &["print", "file2"]);
+    insta::assert_snapshot!(stdout, @"f
+");
+
+    // Can move from ancestor
+    test_env.jj_cmd_success(&repo_path, &["undo"]);
+    let stdout = test_env.jj_cmd_success(&repo_path, &["move", "--from", "@--"]);
+    insta::assert_snapshot!(stdout, @"Working copy now at: c8d83075e8c2 
+");
+    let stdout = test_env.jj_cmd_success(&repo_path, &["log", "-T", template]);
+    insta::assert_snapshot!(stdout, @r###"
+    @ c8d83075e8c2 f
+    o 2c50bfc59c68 e
+    | o caa4d0b23201 c
+    | o 55171e33db26 b
+    |/  
+    o 3db0a2f5b535 a d
+    o 000000000000 
+    "###);
+    // The change from the source has been applied (the file contents were already
+    // "f", as is typically the case when moving changes from an ancestor)
+    let stdout = test_env.jj_cmd_success(&repo_path, &["print", "file2"]);
+    insta::assert_snapshot!(stdout, @"f
+");
+    // The change has been removed from the source (the change pointed to by 'd' was
+    // abandoned)
+    let stdout = test_env.jj_cmd_success(&repo_path, &["log", "-T", template]);
+    insta::assert_snapshot!(stdout, @r###"
+    @ c8d83075e8c2 f
+    o 2c50bfc59c68 e
+    | o caa4d0b23201 c
+    | o 55171e33db26 b
+    |/  
+    o 3db0a2f5b535 a d
+    o 000000000000 
+    "###);
+}
