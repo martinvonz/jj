2720fc636e95442f1307f9091cbad8a906b8f153
Martin von Zweigbergk <martinvonz@google.com>
2022-04-06 11:00:27 -0700

    cli: add a command for printing the contents of a file in a revision
    I'm adding this mostly because it's useful for testing. That's also
    the reason it supports displaying conflicts. I didn't call it `cat`
    like `hg cat` because I haven't found `hg cat` on multiple files
    useful.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### New features
+
+* The new `jj print` command prints the contents of a file in a revision.
+
 ## [0.4.0] - 2022-04-02
 
 ### Breaking changes
---
+++ tests/test_print_command.rs
@@ -0,0 +1,67 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::common::TestEnvironment;
+
+pub mod common;
+
+#[test]
+fn test_print() {
+    let test_env = TestEnvironment::default();
+    test_env.jj_cmd_success(test_env.env_root(), &["init", "repo", "--git"]);
+    let repo_path = test_env.env_root().join("repo");
+
+    std::fs::write(repo_path.join("file1"), "a\n").unwrap();
+    test_env.jj_cmd_success(&repo_path, &["new"]);
+    std::fs::write(repo_path.join("file1"), "b\n").unwrap();
+    std::fs::create_dir(repo_path.join("dir")).unwrap();
+    std::fs::write(repo_path.join("dir").join("file2"), "c\n").unwrap();
+
+    let stdout = test_env.jj_cmd_success(&repo_path, &["print", "file1", "-r", "@-"]);
+    insta::assert_snapshot!(stdout, @"a
+");
+    let stdout = test_env.jj_cmd_success(&repo_path, &["print", "file1"]);
+    insta::assert_snapshot!(stdout, @"b
+");
+    let subdir_file = if cfg!(unix) {
+        "dir/file2"
+    } else {
+        "dir\\file2"
+    };
+    let stdout = test_env.jj_cmd_success(&repo_path, &["print", subdir_file]);
+    insta::assert_snapshot!(stdout, @"c
+");
+    let stdout = test_env.jj_cmd_failure(&repo_path, &["print", "non-existent"]);
+    insta::assert_snapshot!(stdout, @"Error: No such path
+");
+    let stdout = test_env.jj_cmd_failure(&repo_path, &["print", "dir"]);
+    insta::assert_snapshot!(stdout, @"Error: Path exists but is not a file
+");
+
+    // Can print a conflict
+    test_env.jj_cmd_success(&repo_path, &["new"]);
+    std::fs::write(repo_path.join("file1"), "c\n").unwrap();
+    test_env.jj_cmd_success(&repo_path, &["rebase", "-d", "@--"]);
+    let stdout = test_env.jj_cmd_success(&repo_path, &["print", "file1"]);
+    insta::assert_snapshot!(stdout, @r###"
+    <<<<<<<
+    -------
+    +++++++
+    -b
+    +a
+    +++++++
+    c
+    >>>>>>>
+    "###);
+}
