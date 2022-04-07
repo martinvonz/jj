7a14383b9a048f35d72ed47d35b6e02511f152f9
Martin von Zweigbergk <martinvonz@google.com>
2022-04-06 23:20:34 -0700

    cli: let `clap` error out for us on `jj restore -i <file>`

--- tests/test_restore_command.rs
+++ tests/test_restore_command.rs
@@ -169,4 +169,16 @@ fn test_restore_interactive() {
     @@ -1,0 +1,1 @@
     +unrelated
     "###);
+
+    // Combining paths with -i is not yet supported
+    std::fs::write(&edit_script, "").unwrap();
+    let stderr = test_env.jj_cmd_failure(&repo_path, &["restore", "-i", "file2"]);
+    insta::assert_snapshot!(stderr.replace("jj.exe", "jj"), @r###"
+    error: The argument '--interactive' cannot be used with '<PATHS>...'
+
+    USAGE:
+        jj restore --interactive
+
+    For more information try --help
+    "###);
 }
