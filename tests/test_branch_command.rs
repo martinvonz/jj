b626b1840c4af6b9c3b83cd2f217cabcda13842f
Waleed Khan <me@waleedkhan.name>
2022-05-02 13:03:34 -0700

    cli: make `jj branch --revision/--delete/--forget` mutually-exclusive

---
+++ tests/test_branch_command.rs
@@ -0,0 +1,30 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::common::TestEnvironment;
+
+pub mod common;
+
+#[test]
+fn test_branch_mutually_exclusive_actions() {
+    let test_env = TestEnvironment::default();
+    test_env.jj_cmd_success(test_env.env_root(), &["init", "repo", "--git"]);
+    let repo_path = test_env.env_root().join("repo");
+
+    test_env.jj_cmd_failure(&repo_path, &["branch", "--forget", "--delete", "foo"]);
+    test_env.jj_cmd_failure(
+        &repo_path,
+        &["branch", "--delete", "--allow-backwards", "foo"],
+    );
+}
