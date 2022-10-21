96201dbca7716c03742342862a090207659c3b4b
Martin von Zweigbergk <martinvonz@google.com>
2022-10-20 22:13:09 -0700

    demos: replace asciinema by screenshots
    The demos don't need to be animated - the user wouldn't miss anything
    if they skipped to the end. So let's just show the full output so the
    user can read through it at their own pace. We could use plain text,
    but I think the colors are helpful, so I went with screenshots.
    
    Closes #166.

--- demos/demo_git_compat.sh
+++ demos/demo_git_compat.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 . "$(dirname "$0")"/demo_helpers.sh
-parse_args "$@"
 
 new_tmp_dir
 
-run_demo 'Clone a Git repo' '
-run_command "# Clone a Git repo:"
+comment "Clone a Git repo:"
 run_command "jj git clone https://github.com/octocat/Hello-World"
 run_command "cd Hello-World"
-pause 1
-run_command "# Inspect it:"
-pause 1
-run_command "jj log -r '\''all()'\''"
-pause 5
+
+comment "Inspect it:"
+run_command "jj log -r 'all()'"
 run_command "jj diff -r b1"
-pause 2
-run_command "# The repo is backed by the actual Git repo:"
+
+comment "The repo is backed by the actual Git repo:"
 run_command "git --git-dir=.jj/repo/store/git log --graph --all --decorate --oneline"
-'
--- demos/demo_helpers.sh
+++ demos/demo_helpers.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-BASE_DIR=$(realpath "$(dirname "$0")")
-
-UPLOAD=false
-PREVIEW=false
-DEBUG=false
-FAST=false
-parse_args() {
-    for arg in "$@"; do
-        case "$arg" in
-        -h|--help)
-            echo 'Run a given demo.
-Arguments:
-  --preview: Preview the asciicast.
-  --upload: Upload to asciinema (after previewing, if necessary).
-  --debug: Show the asciicast as it is being recorded. Note that what you see
-           will not be exactly the same as what is recorded.
-'
-            exit
-            ;;
-        --upload)
-            UPLOAD=true
-            ;;
-        --preview)
-            PREVIEW=true
-            ;;
-        --debug)
-            DEBUG=true
-            ;;
-        --fast)
-            FAST=true
-            ;;
-        *)
-            echo "Unrecognized argument: $arg"
-            exit 1
-            ;;
-        esac
-    done
-}
 
 new_tmp_dir() {
     local dirname
@@ -47,45 +9,18 @@ new_tmp_dir() {
     trap "rm -rf '$dirname'" EXIT
 }
 
-run_demo() {
-    local title="$1"
-    local test_script="$2"
-    local fast=""
-    if [[ "$FAST" == true ]]; then
-      fast="set send_human {0.005 0.01 1 0.005 0.1}
-proc pause {duration} {
-    sleep [expr \$duration / 10.0]
+run_command() {
+  echo "\$ $@"
+  eval "$@"
 }
-"
-    fi
-    local expect_script="source $BASE_DIR/demo_helpers.tcl
-$fast
-spawn asciinema rec -c \"PS1='$ ' bash --norc\" --title \"$title\"
-expect_prompt
-$test_script
-quit_and_dump_asciicast_path
-"
 
-    if [[ "$DEBUG" == true ]]; then
-        echo "$expect_script" | /usr/bin/env expect
-        return
-    fi
+blank() {
+  echo ""
+}
 
-    echo "Recording demo (terminal size is $(tput cols)x$(tput lines))..."
-    if [[ "$PREVIEW" == 'false' ]]; then
-        echo '(Pass --preview to play the demo automatically once done)'
-    fi
-    local asciicast_path
-    asciicast_path=$(echo "$expect_script" | /usr/bin/env expect | tail -1)
-    echo "$asciicast_path"
-
-    if [[ "$PREVIEW" == 'true' ]]; then
-        asciinema play "$asciicast_path"
-    fi
-    if [[ "$UPLOAD" == 'true' ]]; then
-        if [[ "$PREVIEW" == 'true' ]] && ! confirm "Upload?"; then
-            return
-        fi
-        : asciinema upload "$asciicast_path"
-    fi
+comment() {
+  indented="$(echo "$@"| sed 's/^/# /g')"
+  blank
+  echo -e "\033[0;32m${indented}\033[0m"
+  blank
 }
