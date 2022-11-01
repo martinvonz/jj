8964eafbfa2ee7216cfd4789f05d4cd0478ad0bb
Yuya Nishihara <yuya@tcha.org>
2022-11-01 16:14:21 +0900

    cli: add stub function to parse revset with workspace context
    parse() will take a workspace context to resolve file paths.

--- src/commands.rs
+++ src/commands.rs
@@ -2076,9 +2076,8 @@ fn cmd_log(ui: &mut Ui, command: &CommandHelper, args: &LogArgs) -> Result<(), C
     let workspace_command = command.workspace_helper(ui)?;
 
     let default_revset = ui.settings().default_revset();
-    let revset_expression = revset::optimize(revset::parse(
-        args.revisions.as_ref().unwrap_or(&default_revset),
-    )?);
+    let revset_expression =
+        workspace_command.parse_revset(args.revisions.as_ref().unwrap_or(&default_revset))?;
     let repo = workspace_command.repo();
     let workspace_id = workspace_command.workspace_id();
     let checkout_id = repo.view().get_wc_commit_id(&workspace_id);
