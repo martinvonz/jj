5b10c9aa0a6261be4478b9f7bfd96db28a8339c0
Martin von Zweigbergk <martinvonz@google.com>
2022-11-03 22:23:24 -0700

    local_backend: switch from Protobuf to Thrift
    This migrates the native backend from Protobuf to Thrift since
    Google's Protobuf team does let us import jj into Google's monorepo if
    it uses a third-party Protobuf library.
    
    Since the native backend is not supported, I didn't write any
    migration code for it.
    
    We can't remove `lib/src/protos/store.proto` yet, because it's also
    used by the Git backend (only the `predecessors` and `change_id`
    fields).

---
+++ lib/src/local_backend_model.thrift
@@ -0,0 +1,64 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+struct NormalFile {
+  1: required binary id,
+  2: required bool executable,
+}
+
+union TreeValue {
+  1: NormalFile normal_file,
+  2: binary symlink_id,
+  3: binary tree_id,
+  4: binary conflict_id,
+}
+
+struct TreeEntry {
+  1: required string name,
+  2: required TreeValue value,
+}
+
+struct Tree {
+  1: required list<TreeEntry> entries,
+}
+
+struct Timestamp {
+  1: required i64 millis_since_epoch,
+  2: required i32 tz_offset,
+}
+
+struct Signature {
+  1: required string name,
+  2: required string email,
+  3: required Timestamp timestamp,
+}
+
+struct Commit {
+  1: required list<binary> parents,
+  2: required list<binary> predecessors,
+  3: required binary root_tree,
+  4: required binary change_id,
+  5: required string description,
+  6: required Signature author,
+  7: required Signature committer,
+}
+
+struct ConflictPart {
+  1: required TreeValue content,
+}
+
+struct Conflict {
+  1: required list<ConflictPart> removes,
+  2: required list<ConflictPart> adds,
+}
