24ccd80a5c0b3c4ad7306b7db35adf28a2747a91
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2023-01-15 11:42:53 -0800

    Fix issues with bright color themes
    Fixes https://github.com/martinvonz/jj/issues/528 by changing bright yellow
    to yellow.
    
    Also, stop using `white` or `bright white` which are hardly visible on bright
    themes.
    
    Before (actually before the parent commit):
    
    https://user-images.githubusercontent.com/4123047/212564102-f6e98635-9bde-4392-8b69-5571fdb1303a.png
    
    After:
    
    https://user-images.githubusercontent.com/4123047/212562387-b89a412e-8689-4bdd-9b25-3f5df1a7d31f.png

--- tests/test_commit_template.rs
+++ tests/test_commit_template.rs
@@ -75,8 +75,8 @@ fn test_log_default() {
     // Color
     let stdout = test_env.jj_cmd_success(&repo_path, &["log", "--color=always"]);
     insta::assert_snapshot!(stdout, @r###"
-    @ [1m[38;5;13mffdaa62087a2[39m [38;5;11mtest.user@example.com[39m [38;5;14m2001-02-03 04:05:09.000 +07:00[39m [38;5;13mmy-branch[39m [38;5;12m9de54178d59d[39m[0m
-    | [1m[38;5;10m(empty) [38;5;15mdescription 1[39m[0m
+    @ [1m[38;5;13mffdaa62087a2[39m [38;5;3mtest.user@example.com[39m [38;5;14m2001-02-03 04:05:09.000 +07:00[39m [38;5;13mmy-branch[39m [38;5;12m9de54178d59d[39m[0m
+    | [1m[38;5;10m(empty) [39mdescription 1[0m
     o [38;5;5m9a45c67d3e96[39m [38;5;3mtest.user@example.com[39m [38;5;6m2001-02-03 04:05:08.000 +07:00[39m [38;5;4m4291e264ae97[39m
     | add a file
     o [38;5;5m000000000000[39m  [38;5;6m1970-01-01 00:00:00.000 +00:00[39m [38;5;4m000000000000[39m
@@ -86,8 +86,8 @@ fn test_log_default() {
     // Color without graph
     let stdout = test_env.jj_cmd_success(&repo_path, &["log", "--color=always", "--no-graph"]);
     insta::assert_snapshot!(stdout, @r###"
-    [1m[38;5;13mffdaa62087a2[39m [38;5;11mtest.user@example.com[39m [38;5;14m2001-02-03 04:05:09.000 +07:00[39m [38;5;13mmy-branch[39m [38;5;12m9de54178d59d[39m[0m
-    [1m[38;5;10m(empty) [38;5;15mdescription 1[39m[0m
+    [1m[38;5;13mffdaa62087a2[39m [38;5;3mtest.user@example.com[39m [38;5;14m2001-02-03 04:05:09.000 +07:00[39m [38;5;13mmy-branch[39m [38;5;12m9de54178d59d[39m[0m
+    [1m[38;5;10m(empty) [39mdescription 1[0m
     [38;5;5m9a45c67d3e96[39m [38;5;3mtest.user@example.com[39m [38;5;6m2001-02-03 04:05:08.000 +07:00[39m [38;5;4m4291e264ae97[39m
     add a file
     [38;5;5m000000000000[39m  [38;5;6m1970-01-01 00:00:00.000 +00:00[39m [38;5;4m000000000000[39m
@@ -133,8 +133,8 @@ fn test_log_default_divergence() {
     insta::assert_snapshot!(stdout, @r###"
     o [38;5;1m9a45c67d3e96??[39m [38;5;3mtest.user@example.com[39m [38;5;6m2001-02-03 04:05:10.000 +07:00[39m [38;5;4m8979953d4c67[39m
     | description 2
-    | @ [1m[38;5;9m9a45c67d3e96??[39m [38;5;11mtest.user@example.com[39m [38;5;14m2001-02-03 04:05:08.000 +07:00[39m [38;5;12m7a17d52e633c[39m[0m
-    |/  [1m[38;5;15mdescription 1[39m[0m
+    | @ [1m[38;5;9m9a45c67d3e96??[39m [38;5;3mtest.user@example.com[39m [38;5;14m2001-02-03 04:05:08.000 +07:00[39m [38;5;12m7a17d52e633c[39m[0m
+    |/  [1mdescription 1[0m
     o [38;5;5m000000000000[39m  [38;5;6m1970-01-01 00:00:00.000 +00:00[39m [38;5;4m000000000000[39m
       [38;5;2m(empty) [39m(no description set)
     "###);
