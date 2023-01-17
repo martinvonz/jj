536ac87b11d23548daed34e49be800b3eb2c929a
Michael Forster <michael@forster.pro>
2023-01-17 12:41:33 +0100

    Configure nix development shell environment
    This gets used by `nix develop`, or automatically by `direnv` if you have it
    installed.
    
    The binary versions are pinned to the versions recommended by `contributing.md`.
    
    ```
    >> cargo --version
    cargo 1.60.0 (d1fd9fe 2022-03-01)
    
    >> rustc --version
    rustc 1.60.0 (7737e0b5c 2022-04-04)
    
    >> cargo fmt --version
    rustfmt 1.5.1-nightly (3984bc5 2023-01-17)
    
    >> cargo clippy --version
    clippy 0.1.60 (7737e0b 2022-04-04)
    ```

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
   description = "jujutsu";
 
-  outputs = { self, nixpkgs, ... }:
+  inputs = {
+    # For installing non-standard rustc versions
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, ... }:
     let
       lib = nixpkgs.lib;
       systems = [
@@ -83,7 +89,10 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlays.default ];
+          overlays = [
+            self.overlays.default
+            rust-overlay.overlays.default
+          ];
         };
       in
       {
@@ -103,5 +112,30 @@
           '';
         });
         formatter.${system} = pkgs.nixpkgs-fmt;
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Using the minimal profile with explicit "clippy" extension to avoid
+            # two versions of rustfmt
+            (rust-bin.stable."1.60.0".minimal.override {
+              extensions = [
+                "rust-src" # for rust-analyzer
+                "clippy"
+              ];
+            })
+
+            # The CI checks against the latest nightly rustfmt, so we should too.
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.rustfmt))
+
+            # Required build dependencies
+            openssl
+            pkg-config # to find openssl
+
+            # Additional tools recommended by contributing.md
+            cargo-deny
+            cargo-insta
+            cargo-nextest
+            cargo-watch
+          ];
+        };
       }));
 }
