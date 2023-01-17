e83e44b66597e1a753d88b0c75904b21fe5f1d1f
Benjamin Saunders <ben.e.saunders@gmail.com>
2023-01-16 16:40:22 -0800

    cargo: specify explicit rev for git dep on dag

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
               cargoLock = {
                 lockFile = "${self}/Cargo.lock";
                 outputHashes = {
-                  "dag-0.1.0" = "sha256-Tbta3hkBMvovqz84cu++inVDqrEB2vCGcwTaErOczRw=";
+                  "dag-0.1.0" = "sha256-ZSPdQ5WLx977p36bIbxhbsp2k57DbFaAemGj5mcKavw=";
                   "quickcheck_arbitrary_derive-0.1.0" = "sha256-kUgApls8Uq07QcDrgOLXmAk08fNaEdQtDrnGhP6ff6w=";
                 };
               };
