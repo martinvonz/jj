2af2aca7ffdcc692a81f12e2bb1f000950885bb8
Yuya Nishihara <yuya@tcha.org>
2023-03-05 12:01:30 +0900

    templater: extract indent function to text_util module
    This prepares for adding generic template object for reformatting.

--- src/text_util.rs
+++ src/text_util.rs
@@ -12,10 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::io;
+
+use crate::formatter::{FormatRecorder, Formatter};
+
 pub fn complete_newline(s: impl Into<String>) -> String {
     let mut s = s.into();
     if !s.is_empty() && !s.ends_with('\n') {
         s.push('\n');
     }
     s
+}
+
+/// Indents each line by the given prefix preserving labels.
+pub fn write_indented(
+    formatter: &mut dyn Formatter,
+    recorded_content: &FormatRecorder,
+    mut write_prefix: impl FnMut(&mut dyn Formatter) -> io::Result<()>,
+) -> io::Result<()> {
+    let mut new_line = true;
+    recorded_content.replay_with(formatter, |formatter, data| {
+        for line in data.split_inclusive(|&c| c == b'\n') {
+            if new_line && line != b"\n" {
+                // Prefix inherits the current labels. This is implementation detail
+                // and may be fixed later.
+                write_prefix(formatter)?;
+            }
+            formatter.write_all(line)?;
+            new_line = line.ends_with(b"\n");
+        }
+        Ok(())
+    })
 }
