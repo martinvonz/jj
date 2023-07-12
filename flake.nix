139c9636aa1a0e3e54c270b3913fe915964ec061
Martin von Zweigbergk <martinvonz@google.com>
2023-07-12 09:18:30 -0700

    github: enable the watchman feature in pre-built binaries

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
           pname = "jujutsu";
           version = "unstable-${self.shortRev or "dirty"}";
           buildNoDefaultFeatures = true;
-          buildFeatures = [ "watchman" ];
+          buildFeatures = [ "packaging" ];
           cargoBuildFlags = ["--bin" "jj"]; # don't build and install the fake editors
           useNextest = true;
           src = filterSrc ./. [
