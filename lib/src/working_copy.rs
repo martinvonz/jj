martinvonz/jj

commit 3f97a6da783a748b860fe85ccb44f6324cb588ea

working_copy: avoid adding unchanged values to tree builder
If the value at a path hasn't changed, there's no need to send it over
the channel and have the receiver add it to `TreeBuilder`. I couldn't
measure any performance impact.

Now we should no longer send `TreeValue::Conflict` variants over the
tree entry channel.

lib/src/working_copy.rs
@@ -955,16 +955,24 @@ impl TreeState {
         } else {
             let new_file_type = new_file_state.file_type.clone();
             let current_tree_value = current_tree.path_value(repo_path);
-            let new_tree_value =
-                self.write_path_to_store(repo_path, &disk_path, current_tree_value, new_file_type)?;
-            Ok(Some(new_tree_value))
+            let new_tree_value = self.write_path_to_store(
+                repo_path,
+                &disk_path,
+                &current_tree_value,
+                new_file_type,
+            )?;
+            if new_tree_value.as_resolved() != Some(&current_tree_value) {
+                Ok(Some(new_tree_value))
+            } else {
+                Ok(None)
+            }
         }
     }
     fn write_path_to_store(
         &self,
         repo_path: &RepoPath,
         disk_path: &Path,
-        current_tree_value: Option<TreeValue>,
+        current_tree_value: &Option<TreeValue>,
         file_type: FileType,
     ) -> Result<Merge<Option<TreeValue>>, SnapshotError> {
         let executable = match file_type {
@@ -978,7 +986,7 @@ impl TreeState {
 
         // If the file contained a conflict before and is now a normal file on disk, we
         // try to parse any conflict markers in the file into a conflict.
-        if let Some(TreeValue::Conflict(conflict_id)) = &current_tree_value {
+        if let Some(TreeValue::Conflict(conflict_id)) = current_tree_value {
             let conflict = self.store.read_conflict(repo_path, conflict_id)?;
             if let Some(old_file_ids) = conflict.to_file_merge() {
                 let content = fs::read(disk_path).map_err(|err| SnapshotError::IoError {
@@ -1021,7 +1029,7 @@ impl TreeState {
             let executable = {
                 let () = executable; // use the variable
                 if let Some(TreeValue::File { id: _, executable }) = current_tree_value {
-                    executable
+                    *executable
                 } else {
                     false
                 }

commit 46461c01dc63b578b3f7d2b8d34abcb562a3c1e2

working_copy: write state file even if only mtimes changed
When the main `TreeState::snapshot()` thread doesn't receive any
updated tree entries over the channel, it correctly doesn't write a
new tree. However, it also doesn't write the working copy state file
(`.jj/working_copy/tree_state`). This resulted in performance
regression in 3f97a6da783a7. From that commit, repeated snapshotting
would have to re-read all files from disk because it didn't remember
the updated mtime from the previous time.

This patch fixes the bug by also writing the file if there were any
new file states.

lib/src/working_copy.rs
@@ -645,6 +645,7 @@ impl TreeState {
         let sparse_matcher = self.sparse_matcher();
 
         let fsmonitor_clock_needs_save = fsmonitor_kind.is_some();
+        let mut is_dirty = fsmonitor_clock_needs_save;
         let FsmonitorMatcher {
             matcher: fsmonitor_matcher,
             watchman_clock,
@@ -706,6 +707,7 @@ impl TreeState {
         })?;
         trace_span!("process file states").in_scope(|| {
             while let Ok((path, file_state)) = file_states_rx.recv() {
+                is_dirty = true;
                 self.file_states.insert(path, file_state);
             }
         });
@@ -716,12 +718,13 @@ impl TreeState {
         });
         trace_span!("process deleted files").in_scope(|| {
             for file in &deleted_files {
+                is_dirty = true;
                 self.file_states.remove(file);
                 tree_builder.remove(file.clone());
             }
         });
-        let has_changes = tree_builder.has_overrides();
         trace_span!("write tree").in_scope(|| {
+            is_dirty |= tree_builder.has_overrides();
             self.tree_id = tree_builder.write_tree();
         });
         if cfg!(debug_assertions) {
@@ -734,7 +737,7 @@ impl TreeState {
             assert_eq!(state_paths, tree_paths);
         }
         self.watchman_clock = watchman_clock;
-        Ok(has_changes || fsmonitor_clock_needs_save)
+        Ok(is_dirty)
     }
 
     #[allow(clippy::too_many_arguments)]