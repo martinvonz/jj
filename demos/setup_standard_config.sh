93445a1d29286e95c7abb3cfab6149e34364d175
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2023-08-28 20:57:26 -0700

    demos: run scripts in a fixed environment and record with term-transcript
    Currently, there is no way provided to merely run scripts in
    a fixed environment (without recording).
    
    Short-term TODOs (done in descendant commits):
      - Fix the terminal width
      - Document the script

---
+++ demos/setup_standard_config.sh
@@ -0,0 +1,27 @@
+# This script is meant to be run with `source` from a fresh bash
+# shell. Thus no shebang.
+# shellcheck shell=bash
+set -euo pipefail
+
+JJ_CONFIG=$(mktemp --tmpdir jjconfig-XXXX.toml)
+export JJ_CONFIG
+cat <<'EOF' > "$JJ_CONFIG"
+[user]
+name = "JJ Fan"
+email = "jjfan@example.com"
+
+[operation]
+hostname = "jujube"
+username = "jjfan"
+
+[ui]
+color="always"
+paginate="never"
+EOF
+
+GIT_CONFIG_GLOBAL=$(mktemp --tmpdir gitconfig-XXXX)
+export GIT_CONFIG_GLOBAL
+cat <<'EOF' > "$GIT_CONFIG_GLOBAL"
+[color]
+ui=always
+EOF
