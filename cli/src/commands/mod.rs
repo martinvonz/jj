93780607d9b937b10b1fc45f3ed679aa55b25804
Martin von Zweigbergk <martinvonz@google.com>
2023-09-21 09:40:24 -0700

    cli: `compdef _jj jj` isn't necessary for Zsh completion setup
    We missed this in 8f1dc490 and 38e61511.

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -1130,7 +1130,6 @@ struct UtilCompletionArgs {
     /// autoload -U compinit
     /// compinit
     /// source <(jj util completion --zsh)
-    /// compdef _jj jj
     #[arg(long, verbatim_doc_comment)]
     zsh: bool,
 }
