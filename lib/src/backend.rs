87b24c73a09c0130579eb30f5640c22a5c1c1e99
Yuya Nishihara <yuya@tcha.org>
2023-10-30 06:33:14 +0900

    git_backend: use non-owned str::from_utf8() to decode symlink target
    Just for consistency with the other changes. str::Utf8Error is 2 words long,
    so I removed the boxing.

--- lib/src/backend.rs
+++ lib/src/backend.rs
@@ -251,9 +251,7 @@ pub enum BackendError {
     InvalidUtf8 {
         object_type: String,
         hash: String,
-        // Box to reduce size for other error types that include this one:
-        // https://rust-lang.github.io/rust-clippy/master/index.html#result_large_err
-        source: Box<std::string::FromUtf8Error>,
+        source: std::str::Utf8Error,
     },
     #[error("Object {hash} of type {object_type} not found: {source}")]
     ObjectNotFound {
