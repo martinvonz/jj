42e16bae233ff03e3101cc02c74a63270627ebb6
Austin Seipp <aseipp@pobox.com>
2023-11-01 22:48:43 -0500

    cli: global `--show-heap-stats` argument for EOL heap stats
    Summary: One of the more useful features of mimalloc. In the future this
    interface could be expanded to support printing to a callback, so that it
    could be triggered and dumped e.g. on debugging endpoints for longer running
    processes, when we get there.
    
    Consolidating this into `main.rs` is generally the right choice I think, since
    users of `jj_cli` and related crates will have to write their own `main.rs`
    anyway.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    Change-Id: I6abe4b962bbe7c62ebca97be630b750c

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -12,14 +12,70 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::OnceLock;
+
 #[cfg(feature = "mimalloc")]
 use jj_cbits::mimalloc::MiMalloc;
 use jj_cli::cli_util::CliRunner;
+use jj_cli::command_error::CommandError;
+
+/// Lazy global static. Used only to defer printing mimalloc stats until the
+/// program exits, if set to `true`.
+static PRINT_HEAP_STATS: OnceLock<bool> = OnceLock::new();
+
+#[derive(clap::Args, Clone, Debug)]
+pub struct ShowAllocStats {
+    /// Show memory allocation statistics from the internal heap allocator
+    /// on `stderr`, when the program exits.
+    #[arg(long, global = true)]
+    show_heap_stats: bool,
+}
+
+/// Enable heap statistics for the user interface; should be used with
+/// [`CliRunner::add_global_args`]. Does nothing if the memory allocator is
+/// unused, i.e. `#[global_allocator]` is not set to mimalloc in your program.
+pub fn heap_stats_enable(
+    _ui: &mut jj_cli::ui::Ui,
+    opts: ShowAllocStats,
+) -> Result<(), CommandError> {
+    if opts.show_heap_stats {
+        PRINT_HEAP_STATS.set(true).unwrap();
+    }
+    Ok(())
+}
 
 #[cfg(feature = "mimalloc")]
 #[global_allocator]
 static ALLOC: MiMalloc = MiMalloc;
 
 fn main() -> std::process::ExitCode {
-    CliRunner::init().version(env!("JJ_VERSION")).run()
+    let result = CliRunner::init()
+        // NOTE (aseipp): always attach heap_stats_enable here, even if compiled
+        // without mimalloc; we don't want the test suite or other users to have
+        // to worry about if the command exists or not based on the build
+        // configuration
+        .add_global_args(heap_stats_enable)
+        .version(env!("JJ_VERSION"))
+        .run();
+
+    if PRINT_HEAP_STATS.get() == Some(&true) {
+        #[cfg(feature = "mimalloc")]
+        {
+            // NOTE (aseipp): can we do our own custom printing here? it's kind of ugly
+            eprintln!("========================================");
+            eprintln!("mimalloc memory allocation statistics:\n");
+            jj_cbits::mimalloc::stats_print(&|l| {
+                eprint!("{}", l.to_string_lossy());
+            });
+        }
+
+        #[cfg(not(feature = "mimalloc"))]
+        {
+            eprintln!(
+                "Note: heap statistics requested, but custom memory allocator (mimalloc) is not \
+                 enabled."
+            );
+        }
+    }
+    result
 }
