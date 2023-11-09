53098fc50191ee1aa62f1d807d95f73a557bec98
Yuya Nishihara <yuya@tcha.org>
2023-11-09 10:33:57 +0900

    cli: include branch names in bulk branch creation/update warning
    This will probably help notice missing -r flag. Maybe we can also warn if
    one of the branch names looks like a revset.
    
    https://github.com/martinvonz/jj/discussions/2555

--- cli/tests/test_branch_command.rs
+++ cli/tests/test_branch_command.rs
@@ -27,7 +27,7 @@ fn test_branch_multiple_names() {
     let (stdout, stderr) = test_env.jj_cmd_ok(&repo_path, &["branch", "set", "foo", "bar"]);
     insta::assert_snapshot!(stdout, @"");
     insta::assert_snapshot!(stderr, @r###"
-    warning: Updating multiple branches (2).
+    warning: Updating multiple branches: foo, bar
     "###);
 
     insta::assert_snapshot!(get_log_output(&test_env, &repo_path), @r###"
@@ -44,6 +44,12 @@ fn test_branch_multiple_names() {
     insta::assert_snapshot!(get_log_output(&test_env, &repo_path), @r###"
     @   230dd059e1b0
     ◉   000000000000
+    "###);
+
+    let (stdout, stderr) = test_env.jj_cmd_ok(&repo_path, &["branch", "create", "foo", "bar"]);
+    insta::assert_snapshot!(stdout, @"");
+    insta::assert_snapshot!(stderr, @r###"
+    warning: Creating multiple branches: foo, bar
     "###);
 }
 
