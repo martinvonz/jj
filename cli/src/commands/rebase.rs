f03b65ee3dc8037e79c040248f4638d812c8a852
Yuya Nishihara <yuya@tcha.org>
2023-12-29 18:54:18 +0900

    cli: add missing copyright header to cli/src/commands/*.rs

--- cli/src/commands/rebase.rs
+++ cli/src/commands/rebase.rs
@@ -1,3 +1,4 @@
+// Copyright 2020-2023 The Jujutsu Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
