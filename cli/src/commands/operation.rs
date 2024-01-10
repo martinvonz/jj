63c923afd4c99c7d57131f423e05f2e0accd2964
Yuya Nishihara <yuya@tcha.org>
2024-01-10 17:20:06 +0900

    cli: run "op abandon" without loading repo, reject --at-op
    If indexing failed due to missing commit objects, the repo won't be loadable
    without --ignore-working-copy (at least in colocated environment.) In that
    case, we can use "op abandon" to recover, but we had to work around the failed
    index loading by --ignore-working-copy. Since "op abandon" isn't a repo-level
    command, it's better to bypass working-copy snapshot and import of git refs at
    all.
    
    --at-op is rejected because it's useless and we'll need extra care for "@"
    expression resolution and working-copy updates.

--- cli/src/commands/operation.rs
+++ cli/src/commands/operation.rs
@@ -23,7 +23,8 @@ use jj_lib::op_walk;
 use jj_lib::repo::Repo;
 
 use crate::cli_util::{
-    user_error, user_error_with_hint, CommandError, CommandHelper, LogContentFormat,
+    short_operation_hash, user_error, user_error_with_hint, CommandError, CommandHelper,
+    LogContentFormat,
 };
 use crate::graphlog::{get_graphlog, Edge};
 use crate::operation_templater;
@@ -279,27 +280,37 @@ fn cmd_op_abandon(
     command: &CommandHelper,
     args: &OperationAbandonArgs,
 ) -> Result<(), CommandError> {
-    let mut workspace_command = command.workspace_helper(ui)?;
-    let repo = workspace_command.repo();
-    let current_head_op = repo.operation();
+    // Don't load the repo so that this command can be used to recover from
+    // corrupted repo state.
+    let mut workspace = command.load_workspace()?;
+    let repo_loader = workspace.repo_loader();
+    let op_store = repo_loader.op_store();
+    // It doesn't make sense to create concurrent operations that will be merged
+    // with the current head.
+    let head_op_str = &command.global_args().at_operation;
+    if head_op_str != "@" {
+        return Err(user_error("--at-op is not respected"));
+    }
+    let current_head_op = op_walk::resolve_op_for_load(repo_loader, head_op_str)?;
+    let resolve_op = |op_str| op_walk::resolve_op_at(op_store, &current_head_op, op_str);
     let (abandon_root_op, abandon_head_op) =
         if let Some((root_op_str, head_op_str)) = args.operation.split_once("..") {
             let root_op = if root_op_str.is_empty() {
                 // TODO: Introduce a virtual root operation and use it instead.
-                op_walk::walk_ancestors(slice::from_ref(current_head_op))
+                op_walk::walk_ancestors(slice::from_ref(&current_head_op))
                     .last()
                     .unwrap()?
             } else {
-                workspace_command.resolve_single_op(root_op_str)?
+                resolve_op(root_op_str)?
             };
             let head_op = if head_op_str.is_empty() {
                 current_head_op.clone()
             } else {
-                workspace_command.resolve_single_op(head_op_str)?
+                resolve_op(head_op_str)?
             };
             (root_op, head_op)
         } else {
-            let op = workspace_command.resolve_single_op(&args.operation)?;
+            let op = resolve_op(&args.operation)?;
             let parent_ops: Vec<_> = op.parents().try_collect()?;
             let parent_op = match parent_ops.len() {
                 0 => return Err(user_error("Cannot abandon the root operation")),
@@ -309,7 +320,7 @@ fn cmd_op_abandon(
             (parent_op, op)
         };
 
-    if abandon_head_op == *current_head_op {
+    if abandon_head_op == current_head_op {
         return Err(user_error_with_hint(
             "Cannot abandon the current operation",
             "Run `jj undo` to revert the current operation, then use `jj op abandon`",
@@ -318,9 +329,9 @@ fn cmd_op_abandon(
 
     // Reparent descendants, count the number of abandoned operations.
     let stats = op_walk::reparent_range(
-        repo.op_store().as_ref(),
+        op_store.as_ref(),
         slice::from_ref(&abandon_head_op),
-        slice::from_ref(current_head_op),
+        slice::from_ref(&current_head_op),
         &abandon_root_op,
     )?;
     let [new_head_id]: [OperationId; 1] = stats.new_head_ids.try_into().unwrap();
@@ -334,12 +345,25 @@ fn cmd_op_abandon(
         stats.unreachable_count,
         stats.rewritten_count,
     )?;
-    repo.op_heads_store()
+    repo_loader
+        .op_heads_store()
         .update_op_heads(slice::from_ref(current_head_op.id()), &new_head_id);
     // Remap the operation id of the current workspace. If there were any
     // concurrent operations, user will need to re-abandon their ancestors.
-    let (locked_ws, _) = workspace_command.start_working_copy_mutation()?;
-    locked_ws.finish(new_head_id)?;
+    if !command.global_args().ignore_working_copy {
+        let mut locked_ws = workspace.start_working_copy_mutation()?;
+        let old_op_id = locked_ws.locked_wc().old_operation_id();
+        if old_op_id != current_head_op.id() {
+            writeln!(
+                ui.warning(),
+                "The working copy operation {} is not updated because it differs from the repo {}.",
+                short_operation_hash(old_op_id),
+                short_operation_hash(current_head_op.id()),
+            )?;
+        } else {
+            locked_ws.finish(new_head_id)?
+        }
+    }
     Ok(())
 }
 
