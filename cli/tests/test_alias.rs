0d3e949439a1ece003a71440fc753ae5fc3116e7
jyn <github@jyn.dev>
2024-02-09 17:38:43 -0500

    give a warning when trying to redefine a built-in command
    previously, aliases to built-in commands were silently ignored. this matches git's behavior, but seems unhelpful, especially if the user doesn't know that a command with that name already exists.
    give a warning rather than silently ignoring it.

--- cli/tests/test_alias.rs
+++ cli/tests/test_alias.rs
@@ -164,11 +164,10 @@ fn test_alias_cannot_override_builtin() {
     let repo_path = test_env.env_root().join("repo");
 
     test_env.add_config(r#"aliases.log = ["rebase"]"#);
-    // Alias should be ignored
-    let stdout = test_env.jj_cmd_success(&repo_path, &["log", "-r", "root()"]);
-    insta::assert_snapshot!(stdout, @r###"
-    ◉  zzzzzzzz root() 00000000
-    "###);
+    // Alias should give a warning
+    let (stdout, stderr) = test_env.jj_cmd_ok(&repo_path, &["log", "-r", "root()"]);
+    insta::assert_snapshot!(stdout, @"◉  zzzzzzzz root() 00000000\n");
+    insta::assert_snapshot!(stderr, @"Warning: Cannot define an alias that overrides the built-in command 'log'\n");
 }
 
 #[test]
