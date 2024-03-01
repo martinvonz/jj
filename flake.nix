70e796a6b622dfdf135c353e6765b2066bb6cc62
Benjamin Tan <benjamin@dev.ofcr.se>
2024-03-02 00:43:48 +0800

    git: enable `libgit2`'s OpenSSH feature

--- flake.nix
+++ flake.nix
@@ -112,11 +112,10 @@
             openssh
           ] ++ linuxNativeDeps;
           buildInputs = with pkgs; [
-            openssl zstd libgit2 libssh2
+            openssl zstd libgit2 openssh
           ] ++ darwinDeps;
 
           ZSTD_SYS_USE_PKG_CONFIG = "1";
-          LIBSSH2_SYS_USE_PKG_CONFIG = "1";
           RUSTFLAGS = pkgs.lib.optionalString pkgs.stdenv.isLinux "-C link-arg=-fuse-ld=mold";
           NIX_JJ_GIT_HASH = self.rev or "";
           CARGO_INCREMENTAL = "0";
@@ -177,7 +176,7 @@
           })
 
           # Foreign dependencies
-          openssl zstd libgit2 libssh2
+          openssl zstd libgit2
           pkg-config
 
           # Additional tools recommended by contributing.md
@@ -207,7 +206,6 @@
         shellHook = ''
           export RUST_BACKTRACE=1
           export ZSTD_SYS_USE_PKG_CONFIG=1
-          export LIBSSH2_SYS_USE_PKG_CONFIG=1
 
           export RUSTFLAGS="-Zthreads=0 ${rustLinkFlagsString}"
         '';
