1e83faf4f8234674674330c198f512c1a8cf74d6
Yuya Nishihara <yuya@tcha.org>
2024-03-29 19:48:13 +0900

    tree: remove useless "Backend error" message from TreeMergeError
    I don't think it adds any contextual information. TreeMergeError is somewhat
    similar to BackendError.

--- lib/src/tree.rs
+++ lib/src/tree.rs
@@ -41,7 +41,7 @@ pub enum TreeMergeError {
         source: std::io::Error,
         file_id: FileId,
     },
-    #[error("Backend error")]
+    #[error(transparent)]
     BackendError(#[from] BackendError),
 }
 
