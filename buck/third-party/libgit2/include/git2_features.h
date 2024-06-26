a901830103cead25246e81a98d355db4a004f449
Austin Seipp <aseipp@pobox.com>
2024-06-25 20:29:11 -0500

    buck: build libgit2
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ buck/third-party/libgit2/include/git2_features.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_features_h
+#define INCLUDE_features_h
+
+#define GIT_THREADS 1
+#define GIT_TRACE 1
+#define GIT_HTTPPARSER_BUILTIN 1
+
+#define GIT_ARCH_64 1 // XXX FIXME (aseipp)
+
+#define GIT_SSH 1
+#define GIT_SSH_LIBSSH2 1
+#define GIT_SSH_MEMORY_CREDENTIALS 1
+
+#endif // INCLUDE_features_h
