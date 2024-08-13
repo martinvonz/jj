bb018a54c311cc7d881dc8352d6ef100902b9096
Essien Ita Essien <34972+essiene@users.noreply.github.com>
2024-08-13 19:09:58 +0100

    next/prev: Add config flag to control `prev/next` edit behaviour.
    * We started with a tristate flag where:
        - Auto - Maintain current behaviour. This edits if
          the wc parent is not a head commit. Else, it will
          create a new commit on the parent of the wc in
          the direction of movement.
        - Always - Always edit
        - Never - Never edit, prefer the new+squash workflow.
      However, consensus the review thread is that `auto` mode where we try to infer when to
      switch to `edit mode`, should be removed. So `ui.movement.edit` is a boolean flag now.
        - true: edit mode
        - false: new+squash mode
    * Also add a `--no-edit` flag as the explicit inverse of `--edit` and
      ensure both flags take precedence over the config.
    * Update tests that assumed edit mode inference, to specify `--edit` explicitly.
    
    NOTE: #4302 was squashed into this commit, so see that closed PR for review history.
    
    Part of #3947

--- cli/src/movement_util.rs
+++ cli/src/movement_util.rs
@@ -21,7 +21,7 @@ use jj_lib::commit::Commit;
 use jj_lib::repo::Repo;
 use jj_lib::revset::{RevsetExpression, RevsetFilterPredicate, RevsetIteratorExt};
 
-use crate::cli_util::{short_commit_hash, WorkspaceCommandHelper};
+use crate::cli_util::{short_commit_hash, CommandHelper, WorkspaceCommandHelper};
 use crate::command_error::{user_error, CommandError};
 use crate::ui::Ui;
 
@@ -29,7 +29,15 @@ use crate::ui::Ui;
 pub(crate) struct MovementArgs {
     pub offset: u64,
     pub edit: bool,
+    pub no_edit: bool,
     pub conflict: bool,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct MovementArgsInternal {
+    offset: u64,
+    should_edit: bool,
+    conflict: bool,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -56,12 +64,12 @@ impl Direction {
     fn get_target_revset(
         &self,
         working_commit_id: &CommitId,
-        args: &MovementArgs,
+        args: &MovementArgsInternal,
     ) -> Result<Rc<RevsetExpression>, CommandError> {
         let wc_revset = RevsetExpression::commit(working_commit_id.clone());
         // If we're editing, start at the working-copy commit. Otherwise, start from
         // its direct parent(s).
-        let start_revset = if args.edit {
+        let start_revset = if args.should_edit {
             wc_revset.clone()
         } else {
             wc_revset.parents()
@@ -103,7 +111,7 @@ fn get_target_commit(
     workspace_command: &WorkspaceCommandHelper,
     direction: Direction,
     working_commit_id: &CommitId,
-    args: &MovementArgs,
+    args: &MovementArgsInternal,
 ) -> Result<Commit, CommandError> {
     let target_revset = direction.get_target_revset(working_commit_id, args)?;
     let targets: Vec<Commit> = target_revset
@@ -162,29 +170,29 @@ fn choose_commit<'a>(
 
 pub(crate) fn move_to_commit(
     ui: &mut Ui,
-    workspace_command: &mut WorkspaceCommandHelper,
+    command: &CommandHelper,
     direction: Direction,
     args: &MovementArgs,
 ) -> Result<(), CommandError> {
+    let mut workspace_command = command.workspace_helper(ui)?;
+
     let current_wc_id = workspace_command
         .get_wc_commit_id()
         .ok_or_else(|| user_error("This command requires a working copy"))?;
 
-    let mut args = args.clone();
-    args.edit = args.edit
-        || !&workspace_command
-            .repo()
-            .view()
-            .heads()
-            .contains(current_wc_id);
+    let config_edit_flag = command.settings().config().get_bool("ui.movement.edit")?;
+    let args = MovementArgsInternal {
+        should_edit: args.edit || (!args.no_edit && config_edit_flag),
+        offset: args.offset,
+        conflict: args.conflict,
+    };
 
-    let target = get_target_commit(ui, workspace_command, direction, current_wc_id, &args)?;
+    let target = get_target_commit(ui, &workspace_command, direction, current_wc_id, &args)?;
     let current_short = short_commit_hash(current_wc_id);
     let target_short = short_commit_hash(target.id());
     let cmd = direction.cmd();
-
     // We're editing, just move to the target commit.
-    if args.edit {
+    if args.should_edit {
         // We're editing, the target must be rewritable.
         workspace_command.check_rewritable([target.id()])?;
         let mut tx = workspace_command.start_transaction();
