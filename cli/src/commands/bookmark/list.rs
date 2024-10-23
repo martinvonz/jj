martinvonz/jj

commit 9b5cdf25e60521a7d5673cb44911f63b53f073bc

bookmark: fix doc links
The current links result in a 404. Replace them with the actual location
of the respective pages.

cli/src/commands/bookmark/list.rs
@@ -35,7 +35,7 @@ use crate::ui::Ui;
 /// "+".
 ///
 /// For information about bookmarks, see
-/// https://martinvonz.github.io/jj/docs/bookmarks.md.
+/// https://martinvonz.github.io/jj/latest/bookmarks/.
 #[derive(clap::Args, Clone, Debug)]
 pub struct BookmarkListArgs {
     /// Show all tracking and non-tracking remote bookmarks including the ones
@@ -71,7 +71,7 @@ pub struct BookmarkListArgs {
     ///
     /// All 0-argument methods of the `RefName` type are available as keywords.
     ///
-    /// For the syntax, see https://martinvonz.github.io/jj/latest/docs/templates.md
+    /// For the syntax, see https://martinvonz.github.io/jj/latest/templates/
     #[arg(long, short = 'T')]
     template: Option<String>,
 }

cli/tests/cli-reference@.md.snap
@@ -305,7 +305,7 @@ List bookmarks and their targets
 
 By default, a tracking remote bookmark will be included only if its target is different from the local target. A non-tracking remote bookmark won't be listed. For a conflicted bookmark (both local and remote), old target revisions are preceded by a "-" and new target revisions are preceded by a "+".
 
-For information about bookmarks, see https://martinvonz.github.io/jj/docs/bookmarks.md.
+For information about bookmarks, see https://martinvonz.github.io/jj/latest/bookmarks/.
 
 **Usage:** `jj bookmark list [OPTIONS] [NAMES]...`
 
@@ -327,7 +327,7 @@ For information about bookmarks, see https://martinvonz.github.io/jj/docs/bookma
 
    All 0-argument methods of the `RefName` type are available as keywords.
 
-   For the syntax, see https://martinvonz.github.io/jj/latest/docs/templates.md
+   For the syntax, see https://martinvonz.github.io/jj/latest/templates/
 
 
 

commit 68f1f619b50383bb73ceabc5fa485b9dc6b91d5c

bookmark: fix broken docs URLs that lead to 404 errors
Found a few more broken links beyond those in 9b5cdf2.

cli/src/commands/bookmark/list.rs
@@ -56,7 +56,7 @@ pub struct BookmarkListArgs {
     ///
     /// By default, the specified name matches exactly. Use `glob:` prefix to
     /// select bookmarks by wildcard pattern. For details, see
-    /// https://martinvonz.github.io/jj/docs/revsets.md#string-patterns.
+    /// https://martinvonz.github.io/jj/latest/revsets/#string-patterns.
     #[arg(value_parser = StringPattern::parse)]
     names: Vec<StringPattern>,
 

cli/src/commands/bookmark/mod.rs
@@ -59,7 +59,7 @@ use crate::ui::Ui;
 /// Manage bookmarks
 ///
 /// For information about bookmarks, see
-/// https://martinvonz.github.io/jj/latest/docs/bookmarks.md.
+/// https://martinvonz.github.io/jj/latest/bookmarks.
 #[derive(clap::Subcommand, Clone, Debug)]
 pub enum BookmarkCommand {
     #[command(visible_alias("c"))]

cli/tests/cli-reference@.md.snap
@@ -235,7 +235,7 @@ Apply the reverse of a revision on top of another revision
 
 Manage bookmarks
 
-For information about bookmarks, see https://martinvonz.github.io/jj/latest/docs/bookmarks.md.
+For information about bookmarks, see https://martinvonz.github.io/jj/latest/bookmarks.
 
 **Usage:** `jj bookmark <COMMAND>`
 
@@ -313,7 +313,7 @@ For information about bookmarks, see https://martinvonz.github.io/jj/latest/book
 
 * `<NAMES>` — Show bookmarks whose local name matches
 
-   By default, the specified name matches exactly. Use `glob:` prefix to select bookmarks by wildcard pattern. For details, see https://martinvonz.github.io/jj/docs/revsets.md#string-patterns.
+   By default, the specified name matches exactly. Use `glob:` prefix to select bookmarks by wildcard pattern. For details, see https://martinvonz.github.io/jj/latest/revsets/#string-patterns.
 
 ###### **Options:**
 