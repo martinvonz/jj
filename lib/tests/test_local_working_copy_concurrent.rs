fc03daea6164412012c5389cdf50f887738a2991
Yuya Nishihara <yuya@tcha.org>
2024-11-01 15:11:25 +0900

    testutils: move default_store_factories() to TestEnvironment
    It will capture the TestBackendData mapping.

--- lib/tests/test_local_working_copy_concurrent.rs
+++ lib/tests/test_local_working_copy_concurrent.rs
@@ -26,7 +26,6 @@ use jj_lib::workspace::Workspace;
 use testutils::commit_with_tree;
 use testutils::create_tree;
 use testutils::write_working_copy_file;
-use testutils::TestRepo;
 use testutils::TestWorkspace;
 
 #[test]
@@ -58,7 +57,7 @@ fn test_concurrent_checkout() {
     let mut ws2 = Workspace::load(
         &settings,
         &workspace1_root,
-        &TestRepo::default_store_factories(),
+        &test_workspace1.env.default_store_factories(),
         &default_working_copy_factories(),
     )
     .unwrap();
@@ -75,7 +74,7 @@ fn test_concurrent_checkout() {
     let ws3 = Workspace::load(
         &settings,
         &workspace1_root,
-        &TestRepo::default_store_factories(),
+        &test_workspace1.env.default_store_factories(),
         &default_working_copy_factories(),
     )
     .unwrap();
@@ -113,6 +112,7 @@ fn test_checkout_parallel() {
 
     thread::scope(|s| {
         for tree_id in &tree_ids {
+            let test_env = &test_workspace.env;
             let op_id = repo.op_id().clone();
             let tree_ids = tree_ids.clone();
             let commit = commit_with_tree(repo.store(), tree_id.clone());
@@ -122,7 +122,7 @@ fn test_checkout_parallel() {
                 let mut workspace = Workspace::load(
                     &settings,
                     &workspace_root,
-                    &TestRepo::default_store_factories(),
+                    &test_env.default_store_factories(),
                     &default_working_copy_factories(),
                 )
                 .unwrap();
