786c708df57b02d587a81d59960c8169dfaeacff
Remo Senekowitsch <remo@buenzli.dev>
2024-11-03 23:33:04 +0100

    completion: resolve aliases

--- cli/tests/test_completion.rs
+++ cli/tests/test_completion.rs
@@ -76,3 +76,42 @@ fn test_global_arg_repository_is_respected() {
     );
     insta::assert_snapshot!(stdout, @"aaa");
 }
+
+#[test]
+fn test_aliases_are_resolved() {
+    let test_env = TestEnvironment::default();
+    test_env.jj_cmd_ok(test_env.env_root(), &["git", "init", "repo"]);
+    let repo_path = test_env.env_root().join("repo");
+
+    test_env.jj_cmd_ok(&repo_path, &["bookmark", "create", "aaa"]);
+
+    // user config alias
+    test_env.add_config(r#"aliases.b = ["bookmark"]"#);
+    // repo config alias
+    test_env.jj_cmd_ok(
+        &repo_path,
+        &["config", "set", "--repo", "aliases.b2", "['bookmark']"],
+    );
+
+    let mut test_env = test_env;
+    test_env.add_env_var("COMPLETE", "fish");
+    let test_env = test_env;
+
+    let stdout = test_env.jj_cmd_success(&repo_path, &["--", "jj", "b", "rename", "a"]);
+    insta::assert_snapshot!(stdout, @"aaa");
+
+    let stdout = test_env.jj_cmd_success(&repo_path, &["--", "jj", "b2", "rename", "a"]);
+    insta::assert_snapshot!(stdout, @"aaa");
+}
+
+#[test]
+fn test_completions_are_generated() {
+    let mut test_env = TestEnvironment::default();
+    test_env.add_env_var("COMPLETE", "fish");
+    let stdout = test_env.jj_cmd_success(test_env.env_root(), &[]);
+    // cannot use assert_snapshot!, output contains path to binary that depends
+    // on environment
+    assert!(stdout.starts_with("complete --keep-order --exclusive --command jj --arguments"));
+    let stdout = test_env.jj_cmd_success(test_env.env_root(), &["--"]);
+    assert!(stdout.starts_with("complete --keep-order --exclusive --command jj --arguments"));
+}
