851cf9acd87ae0627325c471e8194e026065fd66
Martin von Zweigbergk <martinvonz@google.com>
2024-12-03 21:45:03 -0800

    rebase: print "Nothing changed" when source set is empty

--- cli/tests/test_rebase_command.rs
+++ cli/tests/test_rebase_command.rs
@@ -127,7 +127,7 @@ fn test_rebase_empty_sets() {
     // TODO: Make all of these say "Nothing changed"?
     let (stdout, stderr) = test_env.jj_cmd_ok(&repo_path, &["rebase", "-r=none()", "-d=b"]);
     insta::assert_snapshot!(stdout, @"");
-    insta::assert_snapshot!(stderr, @"");
+    insta::assert_snapshot!(stderr, @"Nothing changed.");
     let stderr = test_env.jj_cmd_failure(&repo_path, &["rebase", "-s=none()", "-d=b"]);
     insta::assert_snapshot!(stderr, @r###"Error: Revset "none()" didn't resolve to any revisions"###);
     let stderr = test_env.jj_cmd_failure(&repo_path, &["rebase", "-b=none()", "-d=b"]);
@@ -135,7 +135,7 @@ fn test_rebase_empty_sets() {
     // Empty because "b..a" is empty
     let (stdout, stderr) = test_env.jj_cmd_ok(&repo_path, &["rebase", "-b=a", "-d=b"]);
     insta::assert_snapshot!(stdout, @"");
-    insta::assert_snapshot!(stderr, @"");
+    insta::assert_snapshot!(stderr, @"Nothing changed.");
 }
 
 #[test]
